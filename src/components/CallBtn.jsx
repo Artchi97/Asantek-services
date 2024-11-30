f8a16e671c311c20c66a9e1b8ba876a670a52ab4
Artchi97 <artur.wojciechowski97@gmail.com>
2024-11-30 12:06:07 +0100

    some style improvements + seo optimization

--- src/components/CallBtn.jsx
+++ src/components/CallBtn.jsx
@@ -4,7 +4,9 @@ export default function CallBtn() {
   return (
     <div className="call-btn-container">
       <h2 className="need-help-h2">Potrzebujesz sprawnej pomocy?</h2>
-      <button className="call-btn">Zadzwoń: +48 451 186 146</button>
+      <button className="call-btn">
+        <a href="tel:+48451186146">Zadzwoń: +48 451 186 146</a>
+      </button>
     </div>
   );
 }
